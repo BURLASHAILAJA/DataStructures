6a7811066294d8a477ed771b658600efc2616e88
shailajaburla <55321930+BURLASHAILAJA@users.noreply.github.com>
2021-03-06 14:26:03 +0530

    Add files via upload

---
+++ LinkedList Insertion operations.c
@@ -0,0 +1,68 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+struct Node{ 
+int data; 
+struct Node *next; 
+}; 
+void push(struct Node** head_ref, int new_data) 
+{ 
+	/* 1. allocate node */
+	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
+
+	new_node->data = new_data; 
+
+	new_node->next = (*head_ref); 
+
+	(*head_ref) = new_node; 
+} 
+
+void insertAfter(struct Node* prev_node, int new_data) 
+{ 
+	if (prev_node == NULL) 
+	{ 
+	printf("the given previous node cannot be NULL"); 
+	return; 
+	} 
+	struct Node* new_node =(struct Node*) malloc(sizeof(struct Node)); 
+	new_node->data = new_data; 
+	new_node->next = prev_node->next; 
+	prev_node->next = new_node; 
+} 
+
+void append(struct Node** head_ref, int new_data) 
+{ 
+	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
+
+	struct Node *last = *head_ref; /* used in step 5*/
+	new_node->data = new_data; 
+	new_node->next = NULL; 
+	if (*head_ref == NULL) 
+	{ 
+	*head_ref = new_node; 
+	return; 
+	} 
+	while (last->next != NULL) 
+		last = last->next; 
+	last->next = new_node; 
+	return; 
+} 
+void printList(struct Node *node) //traversal function 
+{ 
+while (node != NULL) { 
+	printf(" %d ", node->data); 
+	node = node->next; 
+} 
+} 
+int main() 
+{ 
+struct Node* head = NULL; 
+push(&head, 7); 
+push(&head, 1); 
+push(&head,20);
+append(&head, 4); 
+insertAfter(head->next, 8); 
+insertAfter(head->next->next,90);
+printf("\n Created Linked list is: "); 
+printList(head); 
+return 0; 
+} 
